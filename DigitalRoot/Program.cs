76903a3dc05cad539665d9259372e8bd166d456b
dburke-FootballPools <dominique.burke@footballpools.com>
2022-07-14 16:34:30 +0100

    initial commit for Digital Root code wars kata

---
+++ DigitalRoot/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DigitalRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var digitalRoot16 = DigitalRoot(16); // Should return 7 
+            var digitalRoot456 = DigitalRoot(456); // Should return 6
+            Console.WriteLine($"digitalRoot16 = {digitalRoot16} \n" +
+                $"digitalRoot456 = {digitalRoot456}");
+        }
+
+        public static int DigitalRoot(long n)
+        {
+            while (n > 9)
+            {
+                n = n.ToString().ToCharArray().Sum(x => x - '0');
+            }
+            return ((int)n);
+        }
+    }
+}
