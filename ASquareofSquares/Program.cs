9c812370a01b32653bf6e4f6664d5b7a2b3686ac
dburke-FootballPools <dominique.burke@footballpools.com>
2022-07-29 15:53:18 +0100

    You're a square! CodeWars initial commit

---
+++ ASquareofSquares/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ASquareofSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var attempt1 = Square(-1);
+            var attempt2 = Square(0);
+            var attempt3 = Square(3);
+            var attempt4 = Square(4);
+            var attempt5 = Square(25);
+            var attempt26 = Square(25);
+        }
+
+        // TODO: Given an integral number, determine if it's a square number. 
+        public static bool Square(int numberToCheck)
+        {
+            var temp = Math.Sqrt(numberToCheck);
+            return temp % 1 == 0;
+        }
+    }
+}
