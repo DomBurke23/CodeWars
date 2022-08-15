4057dcd35b9e7989d2d8ee6e7ce0b718e32ac255
dburke-FootballPools <dominique.burke@footballpools.com>
2022-08-15 13:52:52 +0100

    initial commit for reverse words project

---
+++ ReverseWords/ReverseWordsProgram.cs
@@ -0,0 +1,16 @@
+﻿namespace ReverseWords
+{
+    public class ReverseWordsProgram
+    {
+        static void Main(string[] args)
+        {
+            string stringToReverse = "Reverse Words!";
+            var reverseWord = ReverseWords(stringToReverse);
+        }
+
+        public static string ReverseWords(string stringToReverse)
+        {
+            return "";
+        }
+    }
+}
---
+++ UnitTests/ReverseWordsTests.cs
@@ -0,0 +1,17 @@
+﻿using ReverseWords;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ReverseWordsTests
+    {
+        [Theory]
+        [InlineData("This is an example!", "sihT si na !elpmaxe")]
+        [InlineData("double  spaces", "elbuod  secaps")]
+        public static void ReverseWordsTest(string input, string expectedResult)
+        {
+            var actualResult = ReverseWordsProgram.ReverseWords(input);
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
