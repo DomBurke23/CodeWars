ab0d8eacb745fc9cc11d780c728627076ba0b665
dburke-FootballPools <dominique.burke@footballpools.com>
2022-08-15 15:36:27 +0100

    initial commit for logic

--- CreatePhoneNumber/CreatePhoneNumberProgram.cs
+++ CreatePhoneNumber/CreatePhoneNumberProgram.cs
@@ -8,7 +8,9 @@
 
         public static string CreatePhoneNumber(int[] numbers)
         {
-            return null;
+            return $"({numbers[0]}{numbers[1]}{numbers[2]}) " +
+                $"{numbers[3]}{numbers[4]}{numbers[5]}-" +
+                $"{numbers[6]}{numbers[7]}{numbers[8]}{numbers[9]}";
         }
     }
 }
