037581659d20286fa2731655dff9daadba8b7122
dburke-FootballPools <dominique.burke@footballpools.com>
2022-10-10 17:14:55 +0100

    LoopSize
    Commented out because CodeWars hid the class LoopDetector

---
+++ LoopSize/LoopSizeProgram.cs
@@ -0,0 +1,47 @@
+﻿namespace LoopSize
+{
+    public class LoopSizeProgram
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void getLoopSize()
+        { 
+        
+        }
+
+        //public static int getLoopSize(LoopDetector.Node startNode)
+        //{
+        //    LoopDetector.Node slow_pointer = startNode, fast_pointer = startNode;
+
+        //    while (slow_pointer != null && fast_pointer != null
+        //      && fast_pointer.next != null)
+        //    {
+        //        slow_pointer = slow_pointer.next;
+        //        fast_pointer = fast_pointer.next.next;
+
+        //        /* If slow_p and fast_p meet at some point
+        //        then there is a loop */
+        //        if (slow_pointer == fast_pointer)
+        //            return countNodes(slow_pointer);
+        //    }
+
+        //    /* Return 0 to indicate that there is no loop*/
+        //    return 0;
+        //}
+
+        //// Returns count of nodes present in loop.
+        //static int countNodes(LoopDetector.Node currentNode)
+        //{
+        //    int res = 1;
+        //    LoopDetector.Node node = currentNode;
+        //    while (node.next != currentNode)
+        //    {
+        //        res++;
+        //        node = node.next;
+        //    }
+        //    return res;
+        //}
+    }
+}
---
+++ UnitTests/LoopSizeTests.cs
@@ -0,0 +1,36 @@
+using LoopSize;
+using Xunit;
+
+namespace UnitTests
+{
+    public class LoopSizeTests
+    {
+        //[Fact]
+        //public void FourNodesWithLoopSize3()
+        //{
+        //    var n1 = new LoopDetector.Node();
+        //    var n2 = new LoopDetector.Node();
+        //    var n3 = new LoopDetector.Node();
+        //    var n4 = new LoopDetector.Node();
+        //    n1.next = n2;
+        //    n2.next = n3;
+        //    n3.next = n4;
+        //    n4.next = n2;
+        //    Assert.Equal(3, LoopSizeProgram.getLoopSize(n1));
+        //}
+
+        //[Fact]
+        //public void RandomChainNodesWithLoopSize30()
+        //{
+        //    var n1 = LoopDetector.createChain(3, 30);
+        //    Assert.Equal(30, LoopSizeProgram.getLoopSize(n1));
+        //}
+
+        //[Fact]
+        //public void RandomLongChainNodesWithLoopSize1087()
+        //{
+        //    var n1 = LoopDetector.createChain(3904, 1087);
+        //    Assert.Equal(1087, LoopSizeProgram.getLoopSize(n1));
+        //}
+    }
+}
