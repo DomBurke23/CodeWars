baddac9ca4bc6fd216d9a378ad6b769c62a14c46
dburke-FootballPools <dominique.burke@footballpools.com>
2022-11-07 15:37:28 +0000

    inital commit for title case code wars

---
+++ TitleCase/TitleCaseProgram.cs
@@ -0,0 +1,13 @@
+﻿namespace TitleCase;
+
+public class TitleCaseProgram
+{
+    static void Main(string[] args)
+    {
+    }
+
+    public static string TitleCase(string title, string minorWords = "")
+    {
+        return "";
+    }
+}
---
+++ UnitTests/TitleCaseTests.cs
@@ -0,0 +1,18 @@
+﻿using TitleCase;
+using Xunit;
+
+namespace UnitTests
+{
+    public class TitleCaseTests
+    {
+        [Theory]
+        [InlineData("a clash of KINGS", "a an the of", "A Clash of Kings")]
+        [InlineData("THE WIND IN THE WILLOWS", "The In", "The Wind in the Willows")]
+        public static void TitleCaseTest(string title, string minorWords, string expected)
+        {
+            var actualResult = TitleCaseProgram.TitleCase(title,
+                minorWords);
+            Assert.Equal(expected, actualResult);
+        }
+    }
+}
