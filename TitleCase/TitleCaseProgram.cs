5aeaa64fa8918bfc3712e99af5fa190eca128775
dburke-FootballPools <dominique.burke@footballpools.com>
2022-11-07 16:40:45 +0000

    populate title case program to make tests pass

--- TitleCase/TitleCaseProgram.cs
+++ TitleCase/TitleCaseProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace TitleCase;
+﻿using System.Globalization;
+
+namespace TitleCase;
 
 public class TitleCaseProgram
 {
@@ -8,6 +10,37 @@ public class TitleCaseProgram
 
     public static string TitleCase(string title, string minorWords = "")
     {
-        return "";
+        TextInfo textInfo = new CultureInfo("en-UK", false)
+            .TextInfo;
+
+        title = title.ToLower();
+        if (minorWords == null) minorWords = "";
+        minorWords = minorWords.ToLower();
+        var titleList = title.Split(' ');
+        var minorWordList = minorWords.Split(' ');
+
+        var count = 0;
+        foreach (var word in titleList)
+        {
+            if (count == 0)
+            {
+                titleList[count] = textInfo.ToTitleCase(word);
+                count++;
+                continue;
+            }
+            else if (minorWordList.Contains(word))
+            {
+                count++;
+            }
+            else
+            {
+                titleList[count] = textInfo.ToTitleCase(word);
+                count++;
+            }
+        }
+
+        string response = string.Join(" ", titleList);
+
+        return response;
     }
 }
--- UnitTests/TitleCaseTests.cs
+++ UnitTests/TitleCaseTests.cs
@@ -8,6 +8,12 @@ namespace UnitTests
         [Theory]
         [InlineData("a clash of KINGS", "a an the of", "A Clash of Kings")]
         [InlineData("THE WIND IN THE WILLOWS", "The In", "The Wind in the Willows")]
+        [InlineData("aBC deF Ghi", null, "Abc Def Ghi")]
+        [InlineData("ab", "ab", "Ab")]
+        [InlineData("a bc", "bc", "A bc")]
+        [InlineData("a bc", "BC", "A bc")]
+        [InlineData("First a of in", "an often into", "First A Of In")]
+        [InlineData("the QUICK bRoWn fOX", "xyz fox quick the", "The quick Brown fox")]
         public static void TitleCaseTest(string title, string minorWords, string expected)
         {
             var actualResult = TitleCaseProgram.TitleCase(title,
