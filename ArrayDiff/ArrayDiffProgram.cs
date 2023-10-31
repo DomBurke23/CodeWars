47cb9379a30b69e91f4d18a97d28d9a291365755
dburke-FootballPools <dominique.burke@footballpools.com>
2023-10-31 10:34:41 +0000

    kata array diff

---
+++ ArrayDiff/ArrayDiffProgram.cs
@@ -0,0 +1,26 @@
+﻿namespace ArrayDiff
+{
+    public class ArrayDiffProgram
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int[] ArrayDiffsV1(int[] a, int[] b)
+        {
+            var list = a.ToList();
+            foreach (var i in b)
+            {
+                list.RemoveAll(x => x == i);
+            }
+
+            return list.ToArray();
+        }
+
+
+        public static int[] ArrayDiffs(int[] a, int[] b)
+        {
+            return a.Where(n => !b.Contains(n)).ToArray();
+        }
+    }
+}
---
+++ UnitTests/ArrayDiffTests.cs
@@ -0,0 +1,20 @@
+﻿using ArrayDiff;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ArrayDiffTests
+    {
+        [Theory]
+        [InlineData(new int[] { 2 }, new int[] { 1, 2 }, new int[] { 1 })]
+        [InlineData(new int[] { 2, 2 }, new int[] { 1, 2, 2 }, new int[] { 1 })]
+        [InlineData(new int[] { 1 }, new int[] { 1, 2, 2 }, new int[] { 2 })]
+        [InlineData(new int[] { 1, 2, 2 }, new int[] { 1, 2, 2 }, new int[] { })]
+        [InlineData(new int[] { }, new int[] { }, new int[] { 1, 2 })]
+        [InlineData(new int[] { 3 }, new int[] { 1, 2, 3 }, new int[] { 1, 2 })]
+        public void ArrayDiffTest(int[] expected, int[] listA, int[] listB)
+        {
+            Assert.Equal(expected, ArrayDiffProgram.ArrayDiffs(listA, listB));
+        }
+    }
+}
