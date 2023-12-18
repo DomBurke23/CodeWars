54cc75b746d52fa242b60cfe44e546aa00d29bbd
dburke-FootballPools <dominique.burke@footballpools.com>
2023-12-18 16:35:16 +0000

    Don't rely on luck.

---
+++ DontRelyOnLuck/DontRelyOnLuckProgram.cs
@@ -0,0 +1,27 @@
+﻿namespace DontRelyOnLuck
+{
+    public class DontRelyOnLuckProgram
+    {
+        public static int Guess = 1;
+
+        static void Main(string[] args)
+        {
+        }
+
+        public static int DontRelyOnLuck()
+        {
+            return Guess;
+        }
+    }
+}
+
+namespace UnitTests
+{
+    public class Random
+    {
+        public int Next(int min, int max)
+        {
+            return 1;
+        }
+    }
+}
---
+++ UnitTests/DontRelyOnLuckTests.cs
@@ -0,0 +1,16 @@
+﻿using DontRelyOnLuck;
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class DontRelyOnLuckTests
+    {
+        [Fact]
+        public void DontRelyOnLuckTest()
+        {
+            var random = new Random().Next(1, 100 + 1);
+            Assert.Equal(random, DontRelyOnLuckProgram.DontRelyOnLuck());
+        }
+    }
+}
