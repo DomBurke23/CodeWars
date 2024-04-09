8d8b21d500aede2c0456afcaeaf5ec0174b39be9
dsmith-FootballPools <dominique.smith@footballpools.com>
2024-04-09 15:54:00 +0100

    ValidPhoneNumber kata added

---
+++ UnitTests/ValidPhoneNumberTests.cs
@@ -0,0 +1,15 @@
+﻿using ValidPhoneNumber;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ValidPhoneNumberTests
+    {
+        [Fact]
+        public void ValidPhoneNumberTest()
+        {
+            Assert.True(ValidPhoneNumberProgram.ValidPhoneNumberCheck("(123) 456-7890"));
+            Assert.False(ValidPhoneNumberProgram.ValidPhoneNumberCheck("(1111)5X5 2345"));
+        }
+    }
+}
---
+++ ValidPhoneNumber/ValidPhoneNumberProgram.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ValidPhoneNumber
+{
+    /*
+     * Write a function that accepts a string, and returns true if it is in the form of a phone number.
+     * Assume that any integer from 0-9 in any of the spots will produce a valid phone number.
+     * Only worry about the following format:
+     * (123) 456-7890 (don't forget the space after the close parentheses)
+     */
+
+    public partial class ValidPhoneNumberProgram
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool ValidPhoneNumberCheck(string phoneNumber)
+        {
+            return MyRegex().IsMatch(phoneNumber);
+        }
+
+        [GeneratedRegex(@"^\(\d{3}\)\s\d{3}-\d{4}$")]
+        private static partial Regex MyRegex();
+    }
+}
