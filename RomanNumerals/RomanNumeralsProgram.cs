ec4fec191e334bb7c99d9413be9aa9db011a505a
dsmith-FootballPools <dominique.smith@footballpools.com>
2024-06-14 11:18:31 +0100

    initial commit for roman numerals kata

---
+++ RomanNumerals/RomanNumeralsProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace RomanNumerals
+{
+    public partial class RomanNumeralsProgram
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string RomanNumeralsConversion(int input)
+        {
+            return "true";
+        }
+
+    }
+}
---
+++ UnitTests/RomanNumeralsTests.cs
@@ -0,0 +1,19 @@
+﻿using RomanNumerals;
+using Xunit;
+
+namespace UnitTests
+{
+    public class RomanNumeralsTests
+    {
+        [Theory]
+        [InlineData(1, "I")]
+        [InlineData(10, "X")]
+        [InlineData(7, "VII")]
+
+        public static void RomanNumeralsTest(int input, string expectedOutput)
+        {
+            var response = RomanNumeralsProgram.RomanNumeralsConversion(input);
+            Assert.True(response == expectedOutput, $"Response returned - {response}");
+        }
+    }
+}
