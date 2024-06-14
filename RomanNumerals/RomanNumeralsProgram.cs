bbb53228af96be0af16b21fe6c38ce16f1edbc7c
dsmith-FootballPools <dominique.smith@footballpools.com>
2024-06-14 14:34:54 +0100

    Roman Numerals Logic

--- RomanNumerals/RomanNumeralsProgram.cs
+++ RomanNumerals/RomanNumeralsProgram.cs
@@ -1,15 +1,50 @@
 ﻿namespace RomanNumerals
 {
-    public partial class RomanNumeralsProgram
+    public class RomanNumeralsProgram
     {
+        public static Dictionary<int, string> romanDict = new Dictionary<int, string>
+        {
+            { 1000, "M" },
+            { 900, "CM" },
+            { 500, "D" },
+            { 400, "CD" },
+            { 100, "C" },
+            { 90, "XC" },
+            { 50, "L" },
+            { 40, "XL" },
+            { 10, "X" },
+            { 9, "IX" },
+            { 5, "V" },
+            { 4, "IV" },
+            { 1, "I" }
+        };
+
         static void Main(string[] args)
         {
         }
 
-        public static string RomanNumeralsConversion(int input)
+        public static string ConvertToRomanNumerals(int input)
+        {
+            if (input < 1 || input > 3999) throw new ArgumentOutOfRangeException(input.ToString(), "Input must be between 1 and 3999");
+
+            // The dictionary must be in descending order to work, if we put { 1, "I" } first then all tests fail 
+            string result = "";
+            foreach (var pair in romanDict)
+            {
+                while (input >= pair.Key)
+                {
+                    // add the value from dictionary to create the converted value 
+                    result += pair.Value;
+                    // take off the key value from the input value 
+                    input -= pair.Key;
+                }
+            }
+            return result;
+        }
+
+        public static string ConvertFromRomanNumerals(string input)
         {
             return "true";
         }
-
     }
 }
--- UnitTests/RomanNumeralsTests.cs
+++ UnitTests/RomanNumeralsTests.cs
@@ -1,4 +1,5 @@
 ﻿using RomanNumerals;
+using System;
 using Xunit;
 
 namespace UnitTests
@@ -7,13 +8,25 @@ namespace UnitTests
     {
         [Theory]
         [InlineData(1, "I")]
-        [InlineData(10, "X")]
-        [InlineData(7, "VII")]
-
+        [InlineData(1000, "M")]
+        [InlineData(1666, "MDCLXVI")]
+        [InlineData(2022, "MMXXII")]
+        [InlineData(1990, "MCMXC")]
+        [InlineData(2008, "MMVIII")]
+        [InlineData(3999, "MMMCMXCIX")]
         public static void RomanNumeralsTest(int input, string expectedOutput)
         {
-            var response = RomanNumeralsProgram.RomanNumeralsConversion(input);
+            var response = RomanNumeralsProgram.ConvertToRomanNumerals(input);
             Assert.True(response == expectedOutput, $"Response returned - {response}");
+        }
+
+        [Theory]
+        [InlineData(0, "Input must be between 1 and 3999 (Parameter '0')")]
+        [InlineData(4000, "Input must be between 1 and 3999 (Parameter '4000')")]
+        public static void RomanNumeralsExceptionTest(int input, string expectedOutput)
+        {
+            var response = Assert.Throws<ArgumentOutOfRangeException>(() => (RomanNumeralsProgram.ConvertToRomanNumerals(input)));
+            Assert.True(response.Message == expectedOutput, $"Response returned {response.Message}");
         }
     }
 }
