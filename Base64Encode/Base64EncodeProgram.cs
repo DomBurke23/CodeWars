765aedc42f0aac677269dc87dda86514c1da654c
dsmith-FootballPools <dominique.smith@footballpools.com>
2024-06-19 10:40:51 +0100

    logic for base64 encode and decode

--- Base64Encode/Base64EncodeProgram.cs
+++ Base64Encode/Base64EncodeProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace Base64Encode
+﻿using System.Text;
+
+namespace Base64Encode
 {
     public class Base64EncodeProgram
     {
@@ -9,12 +11,16 @@
 
         public static string Encode(string input)
         {
-            return "";
+            byte[] bytes = Encoding.UTF8.GetBytes(input);
+            string encoded = Convert.ToBase64String(bytes);
+            return encoded;
         }
 
         public static string Decode(string input)
         {
-            return "";
+            byte[] bytes = Convert.FromBase64String(input);
+            string decodedString = Encoding.UTF8.GetString(bytes);
+            return decodedString;
         }
     }
 }
